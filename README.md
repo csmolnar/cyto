05120b2ab81625ae3cac0ec3a78708c3023d4592
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-26 19:08:00 +0000

    Deploy cytoai/cyto to github.com/cytoai/cyto.git:gh-pages

--- README.md
+++
@@ -1,1 +0,0 @@
-[![Build Status](https://travis-ci.org/cytoai/cyto.svg?branch=master)](https://travis-ci.org/cytoai/cyto)
--- src/components/SettingsDialog/SettingsDialog.css.js
+++
@@ -1,9 +0,0 @@
-const styles = theme => ({
-  root: {
-    backgroundColor: theme.palette.background.paper,
-    flexGrow: 1
-  },
-  settingsDialog: {}
-});
-
-export default styles;
